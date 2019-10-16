0c7f5bb15c3bdc75b4f83b083ff008d153cd737f
Rodolfo Berlezi <rodolfo.berlezi@compasso.com.br>
2019-10-16 18:18:46 -0300

    App does not have a container. Sass changes and additions. Universe begin.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,7 +15,8 @@ function Home({ history }) {
       const response = await api.get(`/users?user=${user}&password=${password}`);
       setConnectAttempt(false);
 
-      localStorage.setItem('userInfo', response.data);
+      localStorage.setItem('userInfo', response.data._id);
+      localStorage.setItem('universesInfo', response.data.universes);
 
       history.push('/universes');
     } catch (err) {
@@ -27,18 +28,20 @@ function Home({ history }) {
 
   return (
     <>
-      <h1 className="welcome">Welcome to Story Worlds</h1>
-      <h6 className="welcome">Here your imagination becomes alive</h6>
-      <div className="content">
-        { connectAttempt ? <span className="warming">Invalid credentials. Please, try again.</span> : null }
-        <form className="loginForm" onSubmit={handleSubmit}>
-          <label htmlFor="user">Username</label>
-          <input type="text" value={user} id="user" placeholder="Type your username" onChange={event => setUser(event.target.value)} />
-          <label htmlFor="password">Password</label>
-          <input type="password" value={password} id="password" placeholder="Type password" onChange={event => setPassword(event.target.value)} />
-          <button type="submit" className="btn">Sign in</button>
-        </form>
-        <button type="button" className="btn btn-secondary">Sign up</button>
+      <div className="container">
+        <h1 className="welcome">Welcome to Story Worlds</h1>
+        <h6 className="welcome">Here your imagination becomes alive</h6>
+        <div className="content">
+          {connectAttempt ? <span className="warming">Invalid credentials. Please, try again.</span> : null}
+          <form className="loginForm" onSubmit={handleSubmit}>
+            <label htmlFor="user">Username</label>
+            <input type="text" value={user} id="user" placeholder="Type your username" onChange={event => setUser(event.target.value)} />
+            <label htmlFor="password">Password</label>
+            <input type="password" value={password} id="password" placeholder="Type password" onChange={event => setPassword(event.target.value)} />
+            <button type="submit" className="btn">Sign in</button>
+          </form>
+          <button type="button" className="btn btn-secondary">Sign up</button>
+        </div>
       </div>
     </>
   );
--- src/pages/Universe/index.js
+++ src/pages/Universe/index.js
@@ -1,11 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import './styles.scss';
 
-function Universes() {
+import api from '../../services/api';
+
+function Universes({ history }) {
+	const [universe, setUniverse] = useState([]);
+
+	useEffect(() => {
+		async function getUniverse() {
+			const universeId = localStorage.getItem('universesInfo')
+			const universeData = await api.get(`universes/${universeId}`);
+
+			console.log(universeData.data);
+			setUniverse(universeData.data);
+		}
+
+		getUniverse();
+	}, []);
+
 	return (
 		<>
-			<h1 className="welcome universe">Your Universes</h1>
-			
+			<h1 className="welcome">Your Universe</h1>
+			<div className="universe">
+				<div className="grid">
+					<div className="book">
+						<h3>{universe.name}</h3>
+						<img src={universe.image} alt={universe.name} />
+						<p>{universe.description}</p>
+						<p><strong>Races: </strong>{universe.races}</p>
+						<p><strong>Places: </strong>{universe.places}</p>
+					</div>
+					<div></div>
+					<div className="book">{universe.races}</div>
+				</div>
+			</div>
 		</>
 	);
 }
